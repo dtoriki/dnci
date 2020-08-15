07cf70d46663c9f0e1c46ab256ed042550f011cc
Mikhail Loskutov <ms.toriki@mail.ru>
2020-08-15 11:27:23 +0500

    wip: DotNetBuildTool

---
+++ src/DotNetBuildTool/src/DotNetBuildTool.Tool/CliWrapCommandExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using CliWrap;
+
+namespace DotNetBuildTool.Tool
+{
+    internal static class CliWrapCommandExtensions
+    {
+        internal static Stream _stdout = Console.OpenStandardOutput();
+        internal static Stream _stderr = Console.OpenStandardOutput();
+
+        internal static Command ToConsole(this Command command) => command | (_stdout, _stderr);
+    }
+}
